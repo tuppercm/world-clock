e6e2804bc15c465ad0524a684d2aae7b08fe4bf3
tuppercm <103149639+tuppercm@users.noreply.github.com>
2022-11-06 08:00:18 -0500

    Added current location as select option

--- javascript/index.js
+++ javascript/index.js
@@ -28,6 +28,11 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimezone = event.target.value;
+
+  if (cityTimezone === "current") {
+    cityTimezone = moment.tz.guess();
+  }
+
   let cityName = cityTimezone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimezone);
   let citiesElement = document.querySelector("#cities");
